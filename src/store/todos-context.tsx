d8bdae826134b489d4f9faa5df7ee3b748eac929
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-06-21 19:11:50 +0200

    replace with useContext

---
+++ src/store/todos-context.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import Todo from "../models/todo";
+
+type TodosContextObj = {
+  items: Todo[];
+  addTodo: (text: string) => void;
+  removeTodo: (id: string) => void;
+};
+
+type TodosContextProviderProps = {
+  children: React.ReactNode;
+};
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: () => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC<TodosContextProviderProps> = ({
+  children,
+}: TodosContextProviderProps) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  };
+
+  return (
+    <TodosContext.Provider value={contextValue}>
+      {children}
+    </TodosContext.Provider>
+  );
+};
+
+export default TodosContextProvider;
